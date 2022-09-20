dff195ed763c1d3e1aa682c61f9b81d5443174e9
Simon0247 <simon02471@gmail.com>
2022-09-20 16:21:11 +0200

    Sim

---
+++ Eval/Exo 1Eval.js
@@ -0,0 +1,19 @@
+let j;
+let v;
+do {
+    personnes = window.prompt("Entrez votre age");
+    if ( personnes <20) {
+      j++;
+      console.log(personnes);
+    }
+    else if ((personnes >=40 ) && (personnes <=100)) {
+      v++;
+      console.log(v);
+    }
+  }
+  while (personnes < 100);
+
+
+
+
+console.alert ("Il y a " + "" + j + "" + " jeunes\n " + "Il y a " + "" + a + "" + " adulte\n " + " Il y a " + " Il y a " + "" + v + "" +" vieux\n " + " dont "+ " 1 " + "" + " centenaire ");
--- JS/TABLEAUX.js
+++ JS/TABLEAUX.js
@@ -19,61 +19,61 @@
 ///////////////////////Exercice 2
 
 
-function InitTab (){
-     r = window.prompt("Entrez la taille du  tableau");
-     re = new Array();
-    re.length = r ;  
-    console.log(re);
+// function InitTab (){
+//      r = window.prompt("Entrez la taille du  tableau");
+//      re = new Array();
+//     re.length = r ;  
+//     console.log(re);
 
-//     for (i=0; i<r; i++){
-//     re [i] = ;
-//     console.log(re)
-    // }
-}
-InitTab()
-
-
-function SaisieTab (){
-    for (i=0;i<r;i++){
-        let s = window.prompt("Saisir un chiffre  du tableau")
-        re[i] = s 
-        console.log(re);
-    }
-}
-SaisieTab()
+// //     for (i=0; i<r; i++){
+// //     re [i] = ;
+// //     console.log(re)
+//     // }
+// }
+// InitTab()
 
 
-function AfficheTab (){
-    window.alert(re);
-}
-AfficheTab()
+// function SaisieTab (){
+//     for (i=0;i<r;i++){
+//         let s = window.prompt("Saisir un chiffre  du tableau")
+//         re[i] = s 
+//         console.log(re);
+//     }
+// }
+// SaisieTab()
 
 
-
-function RechercheTab (){
-    let v = window.prompt("Saisir le numéro de la case")
-    alert(re[v-1]);
-}
-RechercheTab()
+// function AfficheTab (){
+//     window.alert(re);
+// }
+// AfficheTab()
 
 
 
-function InfoTab ()
-{
-alert("La plus grande valeur du tableau est : " + (Math.max(...re)));
+// function RechercheTab (){
+//     let v = window.prompt("Saisir le numéro de la case")
+//     alert(re[v-1]);
+// }
+// RechercheTab()
 
-let som = 0;
 
-for (i=0; i< re.length;i++){
+
+// function InfoTab ()
+// {
+// alert("La plus grande valeur du tableau est : " + (Math.max(...re)));
+
+// let som = 0;
+
+// for (i=0; i< re.length;i++){
     
-    som = Number(som) + Number(re[i]);
+//     som = Number(som) + Number(re[i]);
     
-}
-alert("Somme = " + som);
-let moy = som/re.length;
-alert("moyenne = " + moy);
-}
-InfoTab()
+// }
+// alert("Somme = " + som);
+// let moy = som/re.length;
+// alert("moyenne = " + moy);
+// }
+// InfoTab()
 
 
 
@@ -83,23 +83,23 @@ InfoTab()
 
 
 
-function bubbleSort(tableau){
+// function bubbleSort(tableau){
    
-let isSorted = false;
-while(!isSorted){
-    isSorted = true;
-    for(let value =0; value<tableau.length; value++){
-    if(value == tableau.length -1)break;
-    if(tableau[value]>re[value+1]){
-    const buffer = tableau[value+1]; 
-    tableau[value+1]=tableau[value];
-    tableau[value] = buffer;
-    isSorted = false;
-}
-}
-}
-}
-// const tableau = [12,11,24,0,32,100,48,72];
-bubbleSort(re);
-alert(re);
-// re.sort();
+// let isSorted = false;
+// while(!isSorted){
+//     isSorted = true;
+//     for(let value =0; value<tableau.length; value++){
+//     if(value == tableau.length -1)break;
+//     if(tableau[value]>re[value+1]){
+//     const buffer = tableau[value+1]; 
+//     tableau[value+1]=tableau[value];
+//     tableau[value] = buffer;
+//     isSorted = false;
+// }
+// }
+// }
+// }
+// // const tableau = [12,11,24,0,32,100,48,72];
+// bubbleSort(re);
+// alert(re);
+// // re.sort();
