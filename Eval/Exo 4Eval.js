91d45853dbb441a2fd90136d21b47c162d730740
Simon0247 <simon02471@gmail.com>
2022-09-28 16:44:26 +0200

    Sim

--- Eval/Exo 4Eval.js
+++ Eval/Exo 4Eval.js
@@ -1,33 +1,40 @@
-let PU = window.prompt("Entrez le prix")
+let pu = window.prompt("Prix unitaire")
 let QTECOM = window.prompt("Quantité")
-let TOT = (PU*QTECOM);
-let pa = window.prompt("Prix à payer")
-alert("pu = " + PU + "/QTECOM= " + QTECOM)
-
+let TOT = pu*QTECOM;
+let remis = TOT * 0.05; 
 let port;
-let p;
+alert("pu = " + pu + "/QTECOM= " + QTECOM)
+
+console.log("PU =" + pu + "/QTECOM=" + QTECOM)
     if (TOT > 99 && TOT < 201){
         let remis = TOT * 0.05;
         TOT = TOT - remis;
         alert("remise 5% (" + remis + ") = " + TOT);
-    }
-    else if (port > 500){
-           (port = 0,02); 
-    }
-    else if (port = 6 + '€'){
+       
         
-    } 
-    if (PU * QTECOM >= 500){
-        (port = 0 - pa)
-
     }
-    // if(quant = 600 + '€'){
-    //     ("la remise = "-60 + '€' && port == 0 ); 
-    // }
+        if(TOT > 99 && TOT < 201){
+            port = 0,05;
+        }
+        if(TOT >200){
 
-    // else if (quant = 501 + '€'){
-    //     ("la remise = ")
-    // }
-     
+            port = 0.1;
+        }
+        
+        if(TOT > 99 && TOT <201){
+            port = 0,05;
+        }
+        else if (TOT > 99 && TOT < 201) {
+            port = 0,1;
+        }
+
+        const total = remis * port; 
+        console.log(TOT);
 
 
+
+    // else if (port = 6 + '€'){
+        
+    // } 
+    
+    // 600 * 1 == 600 - remis(TOT) == (-60 + '€');
