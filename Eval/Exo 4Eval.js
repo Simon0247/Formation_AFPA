309a10b61004a6861a7de1aa31868fc5066373c1
Simon0247 <simon02471@gmail.com>
2022-09-29 16:45:12 +0200

    Sim

--- Eval/Exo 4Eval.js
+++ Eval/Exo 4Eval.js
@@ -1,34 +1,37 @@
 let pu = window.prompt("Prix unitaire")
 let QTECOM = window.prompt("Quantité")
 let TOT = pu*QTECOM;
-let remis = TOT * 0.05; 
-let port;
 alert("pu = " + pu + "/QTECOM= " + QTECOM)
 
 console.log("PU =" + pu + "/QTECOM=" + QTECOM)
-    if (TOT > 99 && TOT < 201){
+    if(TOT > 99 && TOT < 201){
         let remis = TOT * 0.05;
         TOT = TOT - remis;
         alert("remise 5% (" + remis + ") = " + TOT);
        
         
     }
-        if(TOT > 99 && TOT < 201){
-            port = 0,05;
-        }
-        if(TOT >200){
-
-            port = 0.1;
-        }
         
-        if(TOT > 99 && TOT <201){
-            port = 0,05;
-        }
-        else if (TOT > 99 && TOT < 201) {
-            port = 0,1;
+        if (TOT > 200){
+            remis = TOT * 0,1;
+            TOT = TOT - remis;
         }
 
-        const total = remis * port; 
+        let port =0;
+
+         if (TOT < 500){
+            port = TOT * 0.2;
+         }
+
+         if (TOT < 500 && port < 6){
+            port = 6;
+         }
+          
+        remis = TOT * 0.05;
+        TOT = TOT - remis
+        // console.log("remise 5%"(" + remis + ") = "+ TOT")
+
+        const total = TOT - port; 
         console.log(TOT);
 
 
