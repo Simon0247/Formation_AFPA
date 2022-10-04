13a9a420da2f540faa620a099bd4eafbf1ca48a3
Simon0247 <simon02471@gmail.com>
2022-10-04 16:34:13 +0200

    Sim

--- Eval/Exo 4Eval.js
+++ Eval/Exo 4Eval.js
@@ -1,49 +1,54 @@
-let pu = window.prompt("Prix unitaire")
-let QTECOM = window.prompt("Quantité")
+let pu = window.prompt("Prix unitaire");
+let QTECOM = window.prompt("Quantité");
 let TOT = pu*QTECOM;
-let valeur;
-alert("pu = " + pu + "/QTECOM= " + QTECOM)
+let remis;
+alert("pu = " + pu + "/QTECOM= " + QTECOM);
 
-console.log("PU =" + pu + "/QTECOM=" + QTECOM)
-    if(TOT > 99 && TOT < 201){
-        let remis = TOT * 0.05;
+console.log("PU =" + pu + "/QTECOM=" + QTECOM);
+    if(TOT >= 100 && TOT <= 200){
+        remis = TOT * 0.05;
         TOT = TOT - remis;
         alert("remise 5% (" + remis + ") = " + TOT);
-       
         
-    }
+        
+        }
         
         if (TOT > 200){
-            remis = TOT * 0,1;
+            remis = TOT * 0.1;
             TOT = TOT - remis;
         }
 
         let port = 0;
 
-         if (TOT < 500){
-            port = TOT * 0.2;
+         if (TOT <= 500){
+            port = TOT * 0.02;
          }
 
-         if (TOT < 6 && port < 500){
+         if (port < 6 && TOT <= 500){
             port = 6;
          }
-          
-         TOT = TOT * remis/pu;
 
-        // console.log("remise 5%"(" + remis + ") = "+ TOT")
+         else if (TOT > 500){
+            port = 0;
+         }
+
+
+         
+         TOT = TOT + port;
+         
 
         console.log("Le port est de :" + port + "€");
          
-        // remis = document.write( "promoId" ).value;
-         pu = Math.round((valeur * (1 - remis / 100)));
-
 
         console.log("Prix à payer = " + TOT + "€");
 
 
-
-    // else if (port = 6 + '€'){
+                
         
-    // } 
+
+
+        // else if (port = 6 + '€'){
+        
+        // } 
     
-    // 600 * 1 == 600 - remis(TOT) == (-60 + '€');
+        // 600 * 1 == 600 - remis(TOT) == (-60 + '€');
---
+++ Eval/Exo 5Eval.js
@@ -0,0 +1,1 @@
+let Société = document.forms ["RegForm"]["Nom"];
